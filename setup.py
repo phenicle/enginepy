a1b25ffe579d257fe949c8fba20f3a10fdb885b1
phenicle <pheniclebeefheart@gmail.com>
2018-11-11 13:08:16 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
    name='enginepy',
-   version='1.0.3',
+   version='1.0.4',
    description='A simple and powerful python API for running external commands.',
    license="Mozilla Public License 2.0 (MPL 2.0)",
    long_description='A simple and powerful python API for external commands. Arguments toggle the behavior of STDOUT and STDERR. The return code is always available.',
    author='Phenicle',
    author_email='pheniclebeefheart@gmail.com',
-   url="https://github.com/phenicle/eventropy",
+   url="https://github.com/phenicle/enginepy",
    packages=['enginepy',],  #same as name
    install_requires=[], #external packages as dependencies
 )
